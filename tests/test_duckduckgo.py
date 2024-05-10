a5b24c8d8e298d31bc42f8bb211be3eb831e0aa6
tomasz_sas <tomaszsas.dev@gmail.com>
2024-05-10 08:55:55 +0200

    Add wait to duckduckgo test

--- tests/test_duckduckgo.py
+++ tests/test_duckduckgo.py
@@ -3,12 +3,15 @@ import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_searching_in_duckduckgo():
     service = Service(ChromeDriverManager().install())
     chrome_driver = Chrome(service=service)
+    wait = WebDriverWait(chrome_driver, 10)
 
     chrome_driver.get("https://duckduckgo.com/")
     chrome_driver.set_window_size(1920, 1080)
@@ -26,11 +29,12 @@ def test_searching_in_duckduckgo():
 
     # Check there is a proper header in results
     expected_header_title = "4_testers Automaty - Kurs Tester Automatyzujący & AI"
-    headers_in_search_results = chrome_driver.find_elements(By.CSS_SELECTOR, "[data-testid=result-title-a] span")
+    locator_headers_in_search_results = (By.CSS_SELECTOR, "[data-testid=result-title-a] span")
+    wait.until(EC.visibility_of_all_elements_located(locator_headers_in_search_results))
+    headers_in_search_results = chrome_driver.find_elements(*locator_headers_in_search_results)
 
     titles = []
     for title in headers_in_search_results:
-        time.sleep(2)
         titles.append(title.text)
     assert expected_header_title in titles
 
