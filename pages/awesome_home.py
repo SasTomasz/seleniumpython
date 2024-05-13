9db7e2599035140f24de8f193654c96c769b4466
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-05-13 08:05:51 +0200

    Add new method to AwesomeHomePage - check_if_number_of_posts_are_correct

--- pages/awesome_home.py
+++ pages/awesome_home.py
@@ -1,12 +1,21 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class AwesomeHomePage:
     def __init__(self, browser: WebDriver) -> None:
         self.browser = browser
+        self.result_headers_locator = (By.CSS_SELECTOR, "h1")
+        self.wait = WebDriverWait(self.browser, 10)
 
     def go_to_home_page(self):
         self.browser.get("https://awesome-testing.blogspot.com/")
+
+    def check_if_number_of_posts_are_correct(self, number_of_posts: int) -> bool:
+        self.wait.until(EC.visibility_of_all_elements_located(self.result_headers_locator))
+        result_headers = self.browser.find_elements(*self.result_headers_locator)
+        return len(result_headers) == number_of_posts
 
 
--- tests/test_awesometesting.py
+++ tests/test_awesometesting.py
@@ -7,21 +7,17 @@ from pages.awesome_home import AwesomeHomePage
 
 
 def test_post_count(browser):
-    wait = WebDriverWait(browser, 10)
     awesome_home_page = AwesomeHomePage(browser)
-
     awesome_home_page.go_to_home_page()
-
-    result_headers_locator = (By.CSS_SELECTOR, "h1")
-    wait.until(EC.visibility_of_element_located(result_headers_locator))
-    result_headers = browser.find_elements(*result_headers_locator)
-    assert_that(len(result_headers)).is_equal_to(4)
+    assert awesome_home_page.check_if_number_of_posts_are_correct(4)
 
 
 def test_post_count_after_search(browser):
     wait = WebDriverWait(browser, 10)
 
-    browser.get("https://awesome-testing.blogspot.com/")
+    awesome_home_page = AwesomeHomePage(browser)
+
+    awesome_home_page.go_to_home_page()
 
     search_button = browser.find_element(By.CSS_SELECTOR, "input.gsc-search-button")
     search_field = browser.find_element(By.CSS_SELECTOR, "input.gsc-input")
